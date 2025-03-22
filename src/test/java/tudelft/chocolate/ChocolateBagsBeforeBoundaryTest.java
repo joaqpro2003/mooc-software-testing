51bd23b1b3b985fbe60b5908a64dbf668034eb06
MarceloMoscosoFlores <marcelo.moscoso@ucsm.edu.pe>
2025-03-22 17:27:13 -0500

    test(chocolate) "Se agrego nuevas funciones"

--- src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
@@ -7,25 +7,24 @@ public class ChocolateBagsBeforeBoundaryTest {
     @Test
     public void totalIsBiggerThanAmountOfBars() {
         ChocolateBags bags = new ChocolateBags();
-        int result = bags.calculate(1, 1, 10);
-        Assertions.assertEquals(-1, result);
+        int result = bags.calculate(10, 3, 20);
+        Assertions.assertEquals(5, result);
     }
 
     @Test
     public void onlyBigBars() {
-        int result = new ChocolateBags().calculate(5, 3, 10);
-        Assertions.assertEquals(0, result);
+        int result = new ChocolateBags().calculate(2, 3, 18);
+        Assertions.assertEquals(-1, result);
     }
 
-    @Test
-    public void bigAndSmallBars() {
-        int result = new ChocolateBags().calculate(5, 3, 17);
-        Assertions.assertEquals(2, result);
-    }
 
     @Test
     public void onlySmallBars() {
-        int result = new ChocolateBags().calculate(4, 2, 3);
-        Assertions.assertEquals(3, result);
+        int result = new ChocolateBags().calculate(3, 2, 2);
+        Assertions.assertEquals(2, result);
     }
 }
+
+
+
+
